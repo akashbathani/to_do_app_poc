b4deae547a86505fed6ad79372d3832ff3987173
akashbathani <aakashbathani29@gmail.com>
2025-08-25 17:16:04 +0530

    add new file aakash.dart

---
+++ lib/aakash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class Aakash extends StatefulWidget {
+  const Aakash({super.key});
+
+  @override
+  State<Aakash> createState() => _AakashState();
+}
+
+class _AakashState extends State<Aakash> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
